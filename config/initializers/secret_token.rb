ffc00dc09cac2c4fed8c0ecf40ddac948baeeeb4
soufiane <soufianeelhamchi@gmail.com>
2014-03-29 12:31:14 +0000

    app created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AjawApp::Application.config.secret_key_base = '06a697a03a10ee6ac4658e5fe04b9639db020b874f35d62aaa9e48e178338a5df0b36891ed7cf9446e5a88b6faa2f3c150ac5b0e27e8afc2370cf198f6b29036'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AjawApp::Application.config.session_store :cookie_store, key: '_ajaw_app_session'
